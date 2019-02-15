30646690ad9462b5ceba7e728b4307c7a4dde4ae
ajh657 <aahoo01@gmail.com>
2019-02-15 17:24:53 +0200

    SQL scripts fixed and sorted

---
+++ SQL/Henkilöstö.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS henkilot (
+    `Henkilo` VARCHAR(10) CHARACTER SET utf8,
+    `Veronumero` INT
+);
+INSERT INTO henkilot VALUES
+    ('Henkilö 1',999999991114),
+    ('Henkilö 2',999999991244),
+    ('Henkilö 3',999999991258),
+    ('Henkilö 4',999999991183),
+    ('Henkilö 5',999999991187);
+INSERT INTO henkilot VALUES
+    ('Henkilö 6',999999991103),
+    ('Henkilö 7',999999991261);
+INSERT INTO henkilot VALUES
+    ('Henkilö 8',999999991252),
+    ('Henkilö 9',999999991015),
+    ('Henkilö 10',999999991119),
+    ('Henkilö 11',999999991178),
+    ('Henkilö 12',999999991269),
+    ('Henkilö 13',999999991025);
+INSERT INTO henkilot VALUES
+    ('Henkilö 14',999999991265),
+    ('Henkilö 15',999999991270),
+    ('Henkilö 16',999999991179),
+    ('Henkilö 17',999999991247);
+INSERT INTO henkilot VALUES
+    ('Henkilö 18',999999991235),
+    ('Henkilö 19',999999991214);
