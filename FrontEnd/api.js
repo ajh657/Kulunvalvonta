ea7dbc81f6acb4c17af0e2e708a291a96766bdfe
Arttu Höylä <aahoo01@gmail.com>
2019-03-13 12:17:54 +0200

    Progress made mysql

--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -4,16 +4,31 @@ const Express = require("express");
 module.exports = function(req,res) {
     switch (req.params.type) {
         case "login":
-            login(req,res)
+            login(req,res);
             break;
     
         default:
-            res.sendFile(__dirname + "/sites/html/404.html");
+            if(req.method != "GET") {
+                res.sendStatus(404);
+            }else{
+                res.sendFile(__dirname + "/sites/html/404.html");
+            }
             break;
     }
 }
 
 /** @type {Express.RequestHandler} */
 function login(req,res) {
-    var Database = require('./mysql.js')
+    var Database = require('./mysql.js');
+    var sha256 = require('sha256');
+    var passwordHash = sha256(req.body.Password)
+
+    console.log(passwordHash);
+    if (passwordHash == Database.GetPasswordHash(req.body.Username)) {
+        //res.cookie();
+        res.redirect('/');
+    }else{
+        res.sendStatus(403);
+    }
+
 }
--- FrontEnd/mysql.js
+++ FrontEnd/mysql.js
@@ -1,11 +1,20 @@
-module.exports = function GetPasswordHash(username) {
+module.exports.GetPasswordHash = function (username) {
     const mysql = require('mysql');
+    var connection = GetConnection();
+    connection.connect();
+    var result;
+     connection.query('SELECT Password FROM henkilot where Username = ' +connection.escape(username),function (error, results, fields) {
+        result = results[0];
+    });
 
+    while (result == null) {}
 
+    return result;
 }
 
 function GetConnection(params) {
     var Config = require('./Mysql.json')
+    const mysql = require('mysql');
     return mysql.createConnection({
         host     : Config.IP,
         user     : Config.Username,
